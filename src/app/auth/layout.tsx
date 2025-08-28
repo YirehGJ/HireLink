cadf06cf1c99207c115a4300fe529b1e935013b1
ANGEL YIREH GARCIA JARA <2202087@universidad-une.com>
2025-08-28 16:18:59 +0000

    Yo los veo en la esquina superior izquierda no justo en el centro

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,8 +3,8 @@ import { Icons } from '@/components/icons';
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-card/50 p-4">
-      <div className="absolute top-8">
+    <div className="flex min-h-screen w-full items-center justify-center bg-card/50 p-4">
+      <div className="absolute left-4 top-4 md:left-8 md:top-8">
         <Link href="/" className="flex items-center gap-2 text-foreground">
           <Icons.logo className="h-8 w-8" />
           <span className="text-2xl font-bold font-headline tracking-tighter">
