b2d6d6ebcfde58865fdac805f3bc820e5dada694
ANGEL YIREH GARCIA JARA <2202087@universidad-une.com>
2025-09-08 15:56:37 +0000

    Con estos cambios ya deberia ser capaz de poder usar mi app en esta url

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-job-recommendations.ts';
-import '@/ai/flows/extract-skills-from-resume.ts';
-import '@/ai/flows/filter-applications-by-ai-match.ts';
